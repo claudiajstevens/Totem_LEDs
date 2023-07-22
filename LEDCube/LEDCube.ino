895d8ca6c4e9650c510090b4b4d7359e7e842263
Claudia Stevens <claudiajstevens@gmail.com>
2023-07-22 16:22:24 -0700

    "Updated LEDs to be different for the wings"

--- LEDCube/LEDCube.ino
+++ LEDCube/LEDCube.ino
@@ -1,47 +1,96 @@
 #include <FastLED.h>
 
-#define LED_PIN               2
-#define LEDS_PER_SEGMENT      14
+#define LED_PIN               6
+#define LEDS_PER_SEGMENT      7
+#define LEDS_PER_WING_SEGMENT 9
 #define SEGMENTS              12
+#define WING_SEGMENTS         2
 #define BRIGHTNESS            200
-#define NUM_LEDS              LEDS_PER_SEGMENT * SEGMENTS
+#define NUM_CUBE_LEDS         LEDS_PER_SEGMENT * SEGMENTS
+#define NUM_WING_LEDS         LEDS_PER_WING_SEGMENT * WING_SEGMENTS
+#define NUM_LEDS              NUM_CUBE_LEDS + NUM_WING_LEDS
 #define NUM_LEDS_WITH_SAFETY  NUM_LEDS + 1
 
-CRGB source1[NUM_LEDS_WITH_SAFETY];
-CRGB source2[NUM_LEDS_WITH_SAFETY];
-CRGB output[NUM_LEDS_WITH_SAFETY];
+CRGB rawleds[NUM_LEDS];
+CRGBSet leds(rawleds, NUM_LEDS);
+CRGBSet cube1(leds(0, 83));
+CRGBSet cube2(leds(0, 83));
+CRGBSet cubeOutput(leds(0, 83));
+CRGBSet wings1(leds(84, 101));
+CRGBSet wings2(leds(84, 101));
+CRGBSet wingsOutput(leds(84, 101));
+
+struct CRGB * ledarray[] = {leds, cube1, cube2, cubeOutput, wings1, wings2, wingsOutput};
 
 uint8_t blendAmount = 0;
 uint8_t patternCounter = 0;
-uint8_t source1Pattern = 0;
-uint8_t source2Pattern = 1;
+uint8_t source1Pattern = 1;
+uint8_t source2Pattern = 0;
 bool useSource1 = false;
 
+
 void setup() {
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(output, NUM_LEDS_WITH_SAFETY);
+  delay(2000); // power-up saftey delay
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
   Serial.begin(57600);
 }
 
 void loop() {
-  EVERY_N_MILLISECONDS(10) {
-    blend(source1, source2, output, NUM_LEDS, blendAmount);   // Blend between the two sources
+//   fill_solid(ledarray[1], 84, CHSV( 213, 255, 255)); //84 is number of elements in the array
+//   FastLED.show();
+//   delay(700);
+//   fill_solid( ledarray[1], 84, CRGB::Black);
+//   FastLED.show();    
+//
+    
+    
+    //fill_solid(ledarray[4] ,18, CHSV( 213, 255, 255)); //8 is number of elements in the array
 
+
+//FastLED.show();
+//   delay(700);
+//   fill_solid( ledarray[2], 18, CRGB::Black);
+//   FastLED.show();  
+//   delay(700);
+//
+//   fill_solid(ledarray[0], 102, CHSV(213, 255, 255));
+//   FastLED.show();
+//   delay(700);
+//   fill_solid(ledarray[0], 102, CRGB::Black);
+//   FastLED.show();
+//   delay(700);
+
+  EVERY_N_MILLISECONDS(10){
+    blend(ledarray[1], ledarray[2], ledarray[3], NUM_CUBE_LEDS, blendAmount);
+    blend(ledarray[4], ledarray[5], ledarray[6], NUM_WING_LEDS, blendAmount);
+
+        
     if (useSource1) {
       if (blendAmount < 255) blendAmount++;                   // Blend 'up' to source 2
     } else {
       if (blendAmount > 0) blendAmount--;                     // Blend 'down' to source 1
     }
+
   }
 
-  EVERY_N_SECONDS(8) {
+
+
+  EVERY_N_SECONDS(8){
     nextPattern();
   }
 
-  runPattern(source1Pattern, source1);                  // Run both patterns simultaneously
-  runPattern(source2Pattern, source2);
+
+  wingPrettyNoise(ledarray[4]);
+  wingRainbowComet(ledarray[5]);
   
+  runPattern(source1Pattern, ledarray[1]);
+  runPattern(source2Pattern, ledarray[2]);
+
   FastLED.show();
+
+  
+
 }
 
 void nextPattern() {
@@ -73,6 +122,7 @@ void runPattern(uint8_t pattern, CRGB *LEDarray) {
   }
 }
 
+
 uint8_t xyz(uint8_t x, uint8_t y, uint8_t z) {
 
   /* The coordinates of the edges run from 0 - 15. Each edge only has 14 'real' pixels,
@@ -82,7 +132,8 @@ uint8_t xyz(uint8_t x, uint8_t y, uint8_t z) {
    */
   
   uint8_t lps = LEDS_PER_SEGMENT;
-  uint8_t safePx = NUM_LEDS;
+  //uint8_t safePx = NUM_LEDS;
+  uint8_t safePx = NUM_LEDS + 1;
 
   if ((x == 0 || x == lps + 1) && (y == 0 || y == lps + 1) && (z == 0 || z == lps + 1)) return safePx;
 
@@ -133,7 +184,7 @@ void pixels(CRGB *LEDarray) {
     pos = (pos + 1) % ((LEDS_PER_SEGMENT + 1) * 4);
   }
 
-  fadeToBlackBy(LEDarray, NUM_LEDS, 10);
+  fadeToBlackBy(LEDarray, NUM_CUBE_LEDS, 10);
   
 }
 
@@ -153,6 +204,8 @@ void fillRainbow(CRGB *LEDarray) {
   LEDarray[xyz(0, LEDS_PER_SEGMENT + 1, pos)] = CRGB::White;
   LEDarray[xyz(LEDS_PER_SEGMENT + 1, LEDS_PER_SEGMENT + 1, LEDS_PER_SEGMENT - pos)] = CRGB::White;
   LEDarray[xyz(LEDS_PER_SEGMENT + 1, 0, pos)] = CRGB::White;
+
+  
 
   EVERY_N_MILLISECONDS(20) {
     pos = (pos + 1) % LEDS_PER_SEGMENT;
@@ -184,11 +237,11 @@ void rainbowComet(CRGB *LEDarray) {
 
 void randomStar(CRGB *LEDarray) {
   EVERY_N_MILLISECONDS(75) {
-    LEDarray[random16(0, NUM_LEDS)] = CRGB::LightGrey;
+    LEDarray[random16(0, NUM_CUBE_LEDS)] = CRGB::LightGrey;
   }
 
 
-  for (int i = 0; i < NUM_LEDS; i++) {
+  for (int i = 0; i < NUM_CUBE_LEDS; i++) {
     
     // Brightness
     uint8_t bNoise = inoise8(i * 100, millis());
@@ -205,10 +258,39 @@ void randomStar(CRGB *LEDarray) {
     }
   }
   
-  fadeToBlackBy(LEDarray, NUM_LEDS, 5);
+  fadeToBlackBy(LEDarray, NUM_CUBE_LEDS, 5);
 }
 
 
 void prettyNoise(CRGB *LEDarray) {
-  fill_noise16 (LEDarray, NUM_LEDS, 1, 0, 100, 1, 1, 50, millis() / 3, 5);
+  fill_noise16 (LEDarray, NUM_CUBE_LEDS, 1, 0, 100, 1, 1, 50, millis() / 3, 5);
+}
+
+void wingRainbowComet(CRGB *LEDarray) {
+  static uint8_t easeOutVal = 0;
+  static uint8_t easeInVal  = 0;
+  
+
+  // Make pattern appear over two segments
+  uint8_t ledsPerWingSegment = LEDS_PER_WING_SEGMENT;
+  uint8_t wingSegments = WING_SEGMENTS;
+
+  easeOutVal = ease8InOutQuad(easeInVal);
+  easeInVal++;
+
+  uint8_t pos = lerp8by8(0, ledsPerWingSegment, easeOutVal);
+  uint8_t hue =  map(pos, 0, ledsPerWingSegment, 0, 230);
+  
+  LEDarray[pos] = CHSV(hue, 255, 255);
+  fadeToBlackBy(LEDarray, ledsPerWingSegment, 20);
+
+  // Copy to other segments
+  for (int i = 0; i < wingSegments; i++) {
+    memmove8(&LEDarray[ledsPerWingSegment * i], &LEDarray[0], ledsPerWingSegment * sizeof(CRGB));
+  }
+}
+
+
+void wingPrettyNoise(CRGB *LEDarray) {
+  fill_noise16 (LEDarray, NUM_WING_LEDS, 1, 0, 100, 1, 1, 50, millis() / 3, 5);
 }
